ec3bdd060e5183d3bf058c8e51b5311f9f6d2da3
Himanshu Mehra <114086979+SeposTech@users.noreply.github.com>
2023-12-06 12:48:32 +0530

    Initial commit

---
+++ app/src/main/java/com/appweb/newgroceryapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.appweb.newgroceryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
